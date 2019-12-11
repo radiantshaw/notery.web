eb1f3c5573322cb8b25610e1d1a5e5959a150c4e
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-12-11 14:01:32 +0530

    Bind inputs in LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -7,8 +7,18 @@ import {
 } from 'react-bootstrap';
 
 import '../css/LoginForm.css';
+import { useTextInputBinding } from '../hooks';
 
 export default function LoginForm({ isLoading, onSubmit }) {
+  let [email, bindEmail] = useTextInputBinding();
+  let [password, bindPassword] = useTextInputBinding();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    onSubmit(email, password);
+  }
+  
   return (
     <Row>
       <Col
@@ -16,15 +26,25 @@ export default function LoginForm({ isLoading, onSubmit }) {
         md={{ span: 8, offset: 2 }}
         lg={{ span: 6, offset: 3 }}
       >
-        <Form className="login-form">
+        <Form className="login-form" onSubmit={handleSubmit}>
           <Form.Group controlId="email">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" disabled={isLoading} />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              disabled={isLoading}
+              onChange={bindEmail}
+            />
           </Form.Group>
 
           <Form.Group controlId="password">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" disabled={isLoading} />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              disabled={isLoading}
+              onChange={bindPassword}
+            />
           </Form.Group>
 
           <Button variant="primary" type="submit" onSubmit={onSubmit}>
