e672e8512573f53d9de997631dba2e925a062d45
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-12-12 21:48:55 +0530

    Conditionally render

--- src/components/NotePage.jsx
+++ src/components/NotePage.jsx
@@ -8,25 +8,54 @@ import {
   ListGroup
 } from 'react-bootstrap';
 
+import { api } from "../utils";
+
 class NotePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      note: {}
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params } } = this.props;
+    
+    api('GET', '/notes/' + params.noteID).then(data => {
+      this.setState({
+        note: data
+      });
+    });
+  }
+  
   render() {
     return (
       <React.Fragment>
-        <Row className="ml-4 mt-3 mr-4">
-          <Button className="mr-auto">Update</Button>
-          <Button className="ml-auto" variant="danger">Delete</Button>
-        </Row>
+        {this.state.note["type"] !== "reading" ? (
+          <Row className="ml-4 mt-3 mr-4">
+            <Button className="mr-auto">Update</Button>
+            {this.state.note["type"] !== "contributing" ? (
+              <Button className="ml-auto" variant="danger">Delete</Button>
+            ) : null}
+          </Row>
+        ) : null}
         <Row className="ml-1 mt-2 mr-1">
           <Col sm="12" md="12" lg="12">
-            <Form.Control as="textarea" rows="10">
-              Some text
-            </Form.Control>
+            <Form.Control
+              as="textarea"
+              rows="10"
+              defaultValue="Sometext"
+              readOnly={this.state.note["type"] === "reading"}
+            />
           </Col>
         </Row>
-        <Row className="ml-4 mt-3 mr-4">
-          <Button className="mr-auto" variant="dark">Add contributors</Button>
-          <Button className="ml-auto" variant="light">Add readers</Button>
-        </Row>
+        {this.state.note["type"] === "mine" ? (
+          <Row className="ml-4 mt-3 mr-4">
+            <Button className="mr-auto" variant="dark">Add contributors</Button>
+            <Button className="ml-auto" variant="light">Add readers</Button>
+          </Row>
+        ) : null}
         <Row className="ml-1 mt-2 mr-1">
           <Col sm="12" md="6" lg="6">
             <ListGroup>
