03d18e0603cc63c69e5bbfff697fa04145b2a960
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-12-16 12:56:42 +0530

    Add contributors and readers

--- src/components/NotePage.jsx
+++ src/components/NotePage.jsx
@@ -8,9 +8,32 @@ class NotePage extends Component {
   constructor(props) {
     super(props);
 
+    this.handleShare = this.handleShare.bind(this);
+
     this.state = {
       note: {}
     };
+  }
+
+  handleShare(data) {
+    const { match: { params } } = this.props;
+    
+    api('POST', '/shares', {
+      ...data,
+      "id": params.noteID
+    }).then(data => {
+      const type = data["type"]
+      
+      this.setState(prevState => ({
+        note: {
+          ...prevState.note,
+          [type]: [...this.state.note[type], {
+            id: data["id"],
+            email: data["email"]
+          }]
+        }
+      }));
+    });
   }
 
   componentDidMount() {
@@ -27,8 +50,9 @@ class NotePage extends Component {
     return (
       <NoteContainer
         type={this.state.note["type"]}
-        contributors={this.state.note["contributors"]}
-        readers={this.state.note["readers"]}
+        contributors={this.state.note["contributing"]}
+        readers={this.state.note["reading"]}
+        onShare={this.handleShare}
       />
     );
   }
